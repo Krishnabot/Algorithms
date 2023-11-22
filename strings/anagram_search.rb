c0f558b14ae72c23447ce02b340f9d4820b70df1
Krishna <libraryismyparadise@gmail.com>
2023-11-22 20:51:20 +0545

    Making ananogram a module

--- strings/anagram_search.rb
+++ strings/anagram_search.rb
@@ -1,3 +1,4 @@
+module AnagramUtils
   FIRST_TWENTY_SIX_PRIMES = [
     2, 3, 5, 7, 11, 13, 17, 19, 23, 29, 31,
     37, 41, 43, 47, 53, 59, 61, 67, 71, 73,
@@ -14,9 +15,10 @@
   def self.anagrams?(first_word, second_word)
     anagram_number(first_word) == anagram_number(second_word)
   end
+end
 
-# Test Cases:
-puts anagrams?('anagram', 'nag a ram') # true
-puts anagrams?('William Shakespeare', 'I am a weakish speller') # true
-puts anagrams?('Madam Curie', 'Radium came') # true
-puts anagrams?('notagram', 'notaflam') # false
+# Test cases:
+puts AnagramUtils.anagrams?('anagram', 'nag a ram') # true
+puts AnagramUtils.anagrams?('William Shakespeare', 'I am a weakish speller') # true
+puts AnagramUtils.anagrams?('Madam Curie', 'Radium came') # true
+puts AnagramUtils.anagrams?('notagram', 'notaflam') # false
